a4b03f3732fbff582c2b170c307fdbce3be901d1
Inspiron 3567 <huy.ln@classfunc.com>
2020-06-10 23:33:53 +0700

    jhhhh

--- src/SortVisualisation/Model/Sorting/BubbleSort.java
+++ src/SortVisualisation/Model/Sorting/BubbleSort.java
@@ -2,52 +2,76 @@ package SortVisualisation.Model.Sorting;
 
 import SortVisualisation.Model.Pointer;
 
+import java.util.concurrent.Semaphore;
+
 public class BubbleSort extends AbstractSort {
     private boolean counting = false; // are we counting no-swaps from the start index?
     private int noSwaps = 0;
-    private int length;
-    private int pointer = 0;
-
+    private boolean isFinished = false;
+    private int arrLength;
+    private Pointer pointer;
+    private StepThread stepThread;
+    private static final Semaphore blockToStepsSemaphore = new Semaphore(0);
     public BubbleSort(int[] unsortedArray) {
         super(unsortedArray);
-        this.length = unsortedArray.length;
+        this.arrLength = unsortedArray.length;
+        pointer = new Pointer();
     }
     @Override
     public Pointer getPointer() {
-        return new Pointer(new int[]{pointer, pointer + 1});
+        return pointer;
     }
-
     @Override
     public int[] sortOneStep() {
-        if (!isFinished()) {
-            if (sortArray[pointer] > sortArray[pointer + 1]) {
-                // we need to swap, so turn of the counting and reset the noSwaps counter
-                counting = false;
-                noSwaps = 0;
-
-                int swap = sortArray[pointer];
-                sortArray[pointer] = sortArray[pointer + 1];
-                sortArray[pointer + 1] = swap;
-            } else {
-                if (!counting && pointer == 0)
-                    counting = true; // start counting, because we had a no-swap at index 0
-
-                if (counting)
-                    noSwaps++;
-            }
-            pointer++;
-            if (pointer == this.length - 1)
-                pointer = 0; // reset pointer to the start
+        if (stepThread == null) {
+            stepThread = new StepThread();
+            stepThread.start();
         } else {
-            pointer = 0;
+            blockToStepsSemaphore.release(); // release a semaphore permit so the SteppableThread stops blocking
         }
+
         return sortArray;
     }
 
     @Override
     public boolean isFinished() {
-        return noSwaps == this.length;
+        return isFinished;
     }
+    private void swapValues(int index1, int index2) {
+        int temp = sortArray[index1];
+        sortArray[index1] = sortArray[index2];
+        sortArray[index2] = temp;
+    }
+    private class StepThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                for(int i = 0;i<arrLength-1;i++)
+                    for(int j = 0;j<arrLength-i-1;j++)
+                    {
+                        recursiveBubbleSort(sortArray,i,j);
 
+                    }
+                // if we get here, we can assume the quickSort finished
+                isFinished = true;
 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void recursiveBubbleSort(int[] inputArray, int i, int j) throws InterruptedException {
+            if (inputArray == null || inputArray.length == 0 )
+                return;
+            pointer.setCurrent(new int[]{j,j+1});
+            if(inputArray[j]>inputArray[j+1])
+            {
+                swapValues(j,j+1);
+//                pointer.updateIndex(0, leftPointer);
+                blockToStepsSemaphore.acquire();
+
+            }
+            blockToStepsSemaphore.acquire();
+        }
+    }
 }
